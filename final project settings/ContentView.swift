b57ada71d3623793d91f76c1964727cb65dee9d1
Rachel Yoon <rachelyoon789@gmail.com>
2023-07-31 14:38:16 -0700

    Initial Commit

---
+++ final project settings/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  final project settings
+//
+//  Created by Rachel Yoon on 7/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    Text("Edit Profile")
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ final project settings/final_project_settingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_project_settingsApp.swift
+//  final project settings
+//
+//  Created by Rachel Yoon on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct final_project_settingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
